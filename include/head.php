ce8885aeeeeb55aa09db832a965ccdd23cbd6069
Archimandrit <35890919+Archimandrit@users.noreply.github.com>
2018-02-06 01:31:02 +0300

    HZ

--- include/head.php
+++ include/head.php
@@ -1,5 +1,5 @@
 <head>
 	<title><?php echo PROJECT_NAME ?> NAME</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="hw/css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
